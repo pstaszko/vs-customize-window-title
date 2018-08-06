6a6df88ae774e431d3160f822b57db23caf8a287
Erwin Mayer <traderwin@gmail.com>
2018-08-06 23:00:20 +0800

    V3.8.1 2018/08/06 [Stable] - Bugfix.

--- CustomizeVSWindowTitle/Resolvers/SolutionResolver.cs
+++ CustomizeVSWindowTitle/Resolvers/SolutionResolver.cs
@@ -44,9 +44,10 @@ namespace ErwinMayerLabs.RenameVSWindowTitle.Resolvers {
             var sb = (SolutionBuild2)Globals.DTE.Solution.SolutionBuild;
             var names = new List<string>(((Array)sb.StartupProjects).Length);
             foreach (string item in (Array)sb.StartupProjects) {
-                names.Add(Globals.DTE.Solution.Item(item).Name);
+                //names.Add(Globals.DTE.Solution.Item(item).Name); // Does not work if project is nested in solution folder as only the solution folder is part of the Solution.Projects collection. 
+                //If project name is not always equal to project filename, replace with solution here: https://stackoverflow.com/questions/38740773/how-to-get-project-inside-of-solution-folder-in-vsix-project
+                names.Add(item.Substring(0, item.Length - Path.GetExtension(item).Length));
             }
-
             return names;
         }
     }
