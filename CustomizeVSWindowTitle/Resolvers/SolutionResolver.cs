4dc39a65386b76e0430c80a9cb69c96b75b4513f
xScooper <scooper.nr@gmail.com>
2019-08-09 13:34:47 +0200

    Returning the original behavior to StartupProjectNamesResolver, and instead inheriting from it and extending a new one to add the new behavior of stripping out the relative path. This adds a pair of new keywords to the extension: "startupProjectsNamesNonRelative" and "startupProjectsNamesNonRelative:X".

--- CustomizeVSWindowTitle/Resolvers/SolutionResolver.cs
+++ CustomizeVSWindowTitle/Resolvers/SolutionResolver.cs
@@ -20,12 +20,14 @@ namespace ErwinMayerLabs.RenameVSWindowTitle.Resolvers {
     }
 
     public class StartupProjectNamesResolver : TagResolver, ISimpleTagResolver {
-        public StartupProjectNamesResolver() : base(tagNames: new[] { "startupProjectsNames", "startupProjectsNames:X" }) { }
+        public StartupProjectNamesResolver() : this(tagNames: new[] { "startupProjectsNames", "startupProjectsNames:X" }) { }
+        public StartupProjectNamesResolver(IEnumerable<string> tagNames) : base(tagNames: tagNames) { }
 
-        public string TagName { get; } = "startupProjectsNames";
+
+        public virtual string TagName { get; } = "startupProjectsNames";
         static readonly Regex StartupProjectsXRegex = new Regex(@"^startupProjectsNames:(.+)$", RegexOptions.Compiled);
 
-        public string Resolve(AvailableInfo info) {
+        public virtual string Resolve(AvailableInfo info) {
             return string.Join(" & ", GetStartupProjectNames());
         }
 
@@ -40,22 +42,56 @@ namespace ErwinMayerLabs.RenameVSWindowTitle.Resolvers {
             return true;
         }
 
-        private static List<string> GetStartupProjectNames() {
+        protected static List<string> GetStartupProjectNames() {
             var sb = (SolutionBuild2)Globals.DTE.Solution.SolutionBuild;
             var names = new List<string>(((Array)sb.StartupProjects).Length);
             foreach (string item in (Array)sb.StartupProjects) {
-                string modifiedItem = item;
-
-                // Strip out folder path so we end up with just the project name
-                int index = modifiedItem.LastIndexOf("\\");
-                if (index > 0) {
-                    modifiedItem = modifiedItem.Substring(index + 1);
-                }
-				
                 //names.Add(Globals.DTE.Solution.Item(item).Name); // Does not work if project is nested in solution folder as only the solution folder is part of the Solution.Projects collection. 
                 //If project name is not always equal to project filename, replace with solution here: https://stackoverflow.com/questions/38740773/how-to-get-project-inside-of-solution-folder-in-vsix-project
                 names.Add(item.Substring(0, item.Length - Path.GetExtension(item).Length));
             }
+            return names;
+        }
+    }
+
+    public class StartupProjectNamesNonRelativeResolver : StartupProjectNamesResolver {
+        public StartupProjectNamesNonRelativeResolver() : base(tagNames: new[] { "startupProjectsNamesNonRelative", "startupProjectsNamesNonRelative:X" }) { }
+
+        public override string TagName { get; } = "startupProjectsNamesNonRelative";
+        static readonly Regex StartupProjectsXRegex = new Regex(@"^startupProjectsNamesNonRelative:(.+)$", RegexOptions.Compiled);
+
+        public override string Resolve(AvailableInfo info)
+        {
+            return string.Join(" & ", GetStartupProjectNamesNonRelative());
+        }
+
+        public override bool TryResolve(string tag, AvailableInfo availableInfo, out string s)
+        {
+            var m = StartupProjectsXRegex.Match(tag);
+            if (!m.Success)
+            {
+                s = null;
+                return false;
+            }
+
+            s = string.Join(m.Groups[1].Value, GetStartupProjectNamesNonRelative());
+            return true;
+        }
+
+        private static List<string> GetStartupProjectNamesNonRelative() {
+            var names = GetStartupProjectNames();
+
+            for (var i = 0; i < names.Count; i++) {
+                string modifiedName = names[i];
+                // Strip out folder path so we end up with just the project name
+                int index = modifiedName.LastIndexOf("\\");
+                if (index > 0) {
+                    modifiedName = modifiedName.Substring(index + 1);
+                }
+
+                names[i] = modifiedName;
+            }
+
             return names;
         }
     }
