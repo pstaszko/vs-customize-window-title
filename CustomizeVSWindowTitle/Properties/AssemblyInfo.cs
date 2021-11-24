85a6d8fc203ac5d4062f901c95f461a54dcee15e
Paul Staszko <paulstaszko@gmail.com>
2021-11-24 10:43:09 -0600

    ok

--- CustomizeVSWindowTitle/Properties/AssemblyInfo.cs
+++ CustomizeVSWindowTitle/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.1.87.0")]
-[assembly: AssemblyFileVersion("7.1.87.0")]
+[assembly: AssemblyVersion("7.1.88.0")]
+[assembly: AssemblyFileVersion("7.1.88.0")]
