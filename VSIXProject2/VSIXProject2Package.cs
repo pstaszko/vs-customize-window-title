d53ffba6d269caf1f1a5f69be04d74b9063dd366
Paul Staszko <paulstaszko@gmail.com>
2023-04-13 17:05:15 -0500

    ok

---
+++ VSIXProject2/VSIXProject2Package.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.RpcContracts.FileSystem;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSIXProject2
+{
+	/// <summary>
+	/// This is the class that implements the package exposed by this assembly.
+	/// </summary>
+	/// <remarks>
+	/// <para>
+	/// The minimum requirement for a class to be considered a valid package for Visual Studio
+	/// is to implement the IVsPackage interface and register itself with the shell.
+	/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+	/// to do it: it derives from the Package class that provides the implementation of the
+	/// IVsPackage interface and uses the registration attributes defined in the framework to
+	/// register itself and its components with the shell. These attributes tell the pkgdef creation
+	/// utility what data to put into .pkgdef file.
+	/// </para>
+	/// <para>
+	/// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+	/// </para>
+	/// </remarks>
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[Guid(VSIXProject2Package.PackageGuidString)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	[ProvideToolWindow(typeof(ToolWindow1))]
+	public sealed class VSIXProject2Package : AsyncPackage
+	{
+		/// <summary>
+		/// VSIXProject2Package GUID string.
+		/// </summary>
+		public const string PackageGuidString = "03656b80-962e-4c6c-9ef8-983d5f03c326";
+
+		#region Package Members
+
+		/// <summary>
+		/// Initialization of the package; this method is called right after the package is sited, so this is the place
+		/// where you can put all the initialization code that rely on services provided by VisualStudio.
+		/// </summary>
+		/// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+		/// <param name="progress">A provider for progress updates.</param>
+		/// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			// When initialized asynchronously, the current thread may be a background thread at this point.
+			// Do any initialization that requires the UI thread after switching to the UI thread.
+			var q = @"c:\temp\x.txt";
+
+			await System.Threading.Tasks.Task.Run(() => System.IO.File.WriteAllText(q, "a"));
+			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+			await Command2.InitializeAsync(this);
+			await ToolWindow1Command.InitializeAsync(this);
+			//await Command1.InitializeAsync(this);
+		}
+		protected override object GetService(Type serviceType)
+		{
+			System.IO.File.WriteAllText(@"c:\temp\xxxx.txt", "a");
+			return base.GetService(serviceType);
+		}
+		protected override string GetToolWindowTitle(Type toolWindowType, int id)
+		{
+			System.IO.File.WriteAllText(@"c:\temp\xxx.txt", "a");
+			return base.GetToolWindowTitle(toolWindowType, id);
+		}
+		protected override Task OnAfterPackageLoadedAsync(CancellationToken cancellationToken)
+		{
+			System.IO.File.WriteAllText(@"c:\temp\xx.txt", "a");
+			return base.OnAfterPackageLoadedAsync(cancellationToken);
+		}
+		#endregion
+	}
+}
